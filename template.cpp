8be87b28bf56ccfda6dff77454f167a051ebe172
MeetSinojia <71940146+MeetSinojia@users.noreply.github.com>
2022-10-17 18:50:36 +0530

    Code Template

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sortt(v) sort(v.begin(), v.end())
+#define sorttd(a) sort(a.begin(), a.end(), greater<int>())
+#define fo(i, n) for (i = 0; i < n; i++)
+#define pb push_back
+#define deb(x) cout << #x << '=' << x << endl
+typedef vector<int> vi;
+typedef vector<ll> vl;
+const int mod = 1'000'000'007;
+
+void solve()
+{
+    int i = 0, j = 0, n = 0, m = 0, a[n];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
